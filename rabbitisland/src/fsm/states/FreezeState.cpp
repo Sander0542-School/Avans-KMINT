55e34b570f7d9f963ff052c2ff4edeacd4905242
Sander Jochems <sanderjochems@hotmail.nl>
2022-01-12 13:57:42 +0100

    Update FreezeState.cpp

--- rabbitisland/src/fsm/states/FreezeState.cpp
+++ rabbitisland/src/fsm/states/FreezeState.cpp
@@ -3,3 +3,16 @@
 #include "kmint/rabbitisland/mister.hpp"
 
 using namespace fsm::states;
+
+template<>
+void FreezeState<kmint::rabbitisland::dog>::Enter()
+{
+    _totalDuration = kmint::from_seconds(0);
+    Data()->IsHunting(false);
+}
+
+template<>
+void FreezeState<kmint::rabbitisland::dog>::Exit()
+{
+    Data()->Sleep();
+}
