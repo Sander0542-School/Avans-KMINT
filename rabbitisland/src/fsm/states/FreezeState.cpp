6686a728227190bf5eef8453e010cb843a4eaa6c
Sander Jochems <sanderjochems@hotmail.nl>
2022-01-12 13:57:35 +0100

    Fix

--- rabbitisland/src/fsm/states/FreezeState.cpp
+++ rabbitisland/src/fsm/states/FreezeState.cpp
@@ -3,20 +3,3 @@
 #include "kmint/rabbitisland/mister.hpp"
 
 using namespace fsm::states;
-
-template<typename Ty_>
-void FreezeState<Ty_>::Enter()
-{
-    _totalDuration = kmint::from_seconds(0);
-}
-
-template<typename Ty_>
-void FreezeState<Ty_>::Exit()
-{
-}
-
-template<>
-void FreezeState<kmint::rabbitisland::dog>::Exit()
-{
-    Data()->Sleep();
-}
